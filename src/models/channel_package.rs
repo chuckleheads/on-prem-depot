4ab34f4b2b8af9cebc678670b639dd2b8d9a8ce4
Elliott Davis <elliott@excellent.io>
2018-04-05 06:54:38 -0500

    Add blank for channel_package
    Signed-off-by: Elliott Davis <elliott@excellent.io>

--- src/models/channel_package.rs
+++ src/models/channel_package.rs
@@ -1,45 +1,106 @@
-// use diesel;
-// use diesel::PgConnection;
-// use diesel::prelude::*;
-// use chrono::NaiveDateTime;
-// use schema::origin_channels;
+use diesel;
+use diesel::PgConnection;
+use diesel::prelude::*;
+use chrono::NaiveDateTime;
+use schema::{origin_channel_packages, origins};
 
-// #[derive(Debug, Serialize, Queryable)]
-// pub struct ChannelPackage {
-//     pub id: i64,
-//     pub origin_id: i64,
-//     pub owner_id: i64,
-//     pub name: String,
-//     pub created_at: Option<NaiveDateTime>,
-//     pub update_at: Option<NaiveDateTime>,
-// }
+#[derive(Debug, Serialize, Queryable)]
+pub struct ChannelPackage {
+    pub channel_id: i64,
+    pub package_id: i64,
+    pub created_at: Option<NaiveDateTime>,
+    pub update_at: Option<NaiveDateTime>,
+}
 
-// #[derive(Debug, Deserialize, Insertable)]
-// #[table_name = "origin_channels"]
-// pub struct NewChannelPackage {
-//     pub name: String,
-//     pub owner_id: i64,
-//     pub default_package_visibility: Option<String>,
-// }
+#[derive(Debug, Deserialize, Insertable)]
+#[table_name = "origin_channel_packages"]
+pub struct NewChannelPackage {
+    pub channel_id: i64,
+    pub package_id: i64,
+}
 
-// #[derive(Debug, Deserialize, Insertable)]
-// #[table_name = "origin_channels"]
-// pub struct UpdateChannelPackage {
-//     pub default_package_visibility: String,
-// }
+// can't implement this until we implement Package
 
-// impl Channel {
-//     pub fn insert(channel: &NewChannel, conn: &PgConnection) -> QueryResult<Channel> {
+// impl ChannelPackage {
+//     pub fn get(
+//         origin: &str,
+//         channel: &str,
+//         pkg: &str,
+//         conn: &PgConnection,
+//     ) -> QueryResult<Package> {
+//         unimplemented!()
+//     }
+
+//     pub fn insert(channel: &NewChannel, conn: &PgConnection) -> QueryResult<ChannelPackage> {
 //         diesel::insert_into(origin_channels::table)
 //             .values(channel)
 //             .get_result(conn)
 //     }
 
-//     pub fn list(origin: &str, channel: &str, conn: &PgConnection) -> QueryResult<Vec<Channel>> {
+//     pub fn list(
+//         origin: &str,
+//         channel: &str,
+//         package: &str,
+//         conn: &PgConnection,
+//     ) -> QueryResult<Vec<Package>> {
 //         unimplemented!()
 //     }
 
-//     pub fn delete(origin: &str, channel: &str, conn: &PgConnection) -> QueryResult<Channel> {
+//     pub fn latest(
+//         origin: &str,
+//         channel: &str,
+//         package: &str,
+//         conn: &PgConnection,
+//     ) -> QueryResult<Vec<Package>> {
+//         unimplemented!()
+//     }
+
+//     pub fn version(
+//         origin: &str,
+//         channel: &str,
+//         package: &str,
+//         version: &str,
+//         conn: &PgConnection,
+//     ) -> QueryResult<Vec<Package>> {
+//         unimplemented!()
+//     }
+
+//     pub fn version_latest(
+//         origin: &str,
+//         channel: &str,
+//         package: &str,
+//         version: &str,
+//         conn: &PgConnection,
+//     ) -> QueryResult<Vec<Package>> {
+//         unimplemented!()
+//     }
+
+//     pub fn version_release(
+//         origin: &str,
+//         channel: &str,
+//         package: &str,
+//         version: &str,
+//         conn: &PgConnection,
+//     ) -> QueryResult<Vec<Package>> {
+//         unimplemented!()
+//     }
+
+//     pub fn promote(
+//         origin: &str,
+//         channel: &str,
+//         package: &str,
+//         version: &str,
+//         conn: &PgConnection,
+//     ) -> QueryResult<Vec<Package>> {
+//         unimplemented!()
+//     }
+//     pub fn demote(
+//         origin: &str,
+//         channel: &str,
+//         package: &str,
+//         version: &str,
+//         conn: &PgConnection,
+//     ) -> QueryResult<Vec<Package>> {
 //         unimplemented!()
 //     }
 // }
--- src/models/mod.rs
+++ src/models/mod.rs
@@ -1,3 +1,4 @@
 pub mod origin;
 pub mod origin_keys;
 pub mod channel;
+pub mod channel_package;
